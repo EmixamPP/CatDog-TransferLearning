0bfffb5e2139bcf9f27c52f6acf90abb721e7f32
EmixamPP <dev@emixam.be>
2023-04-15 11:45:04 +0200

    fix deprecated

--- Cat_or_Dog/train.py
+++ Cat_or_Dog/train.py
@@ -17,6 +17,8 @@ from tensorflow.keras.layers import (
     Flatten,
     Dropout,
 )
+for device in tf.config.experimental.list_physical_devices('GPU'):
+    tf.config.experimental.set_memory_growth(device, True)
 
 import argparse
 
